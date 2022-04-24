d29786ef5dcfbc41d3d67e28a127541a9188c2c1
Ezequiel <kieltwogz@gmail.com>
2022-04-24 18:16:55 -0300

    fix

--- src/lib/firebase/firebase-config.ts
+++ src/lib/firebase/firebase-config.ts
@@ -7,6 +7,7 @@ import { getDatabase, Database } from 'firebase/database'
 import type { Auth } from 'firebase/auth'
 
 // Sets config.
+console.log(JSON.parse(process.env['FIREBASE_CONFIG'] || '{}'))
 const firebaseConfig = JSON.parse(process.env['FIREBASE_CONFIG'] || '{}')
 
 // Export all new instances to application.
@@ -16,4 +17,5 @@ export const initializedApp: FirebaseApp = initializeApp(firebaseConfig)
 export const initializedDatabase: Database = getDatabase(initializedApp)
 export const initializedAuth: Auth = getAuth(initializedApp)
 
+console.log(process.env['RECAPTCHA_KEY'] || '')
 export const reCaptchaKey = process.env['RECAPTCHA_KEY'] || ''
